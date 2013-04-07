7fbcbe2cbea77f08f8708236d417d87c94bb2679
dpatro <patro@sunglass.io>
2013-04-07 18:17:09 +0530

    DB updates. Added New Tables

--- projdb.py
+++ projdb.py
@@ -6,8 +6,9 @@ from google.appengine.ext import db
 
 class User(db.Model):
     name = db.StringProperty()
-    email = db.StringProperty()
+    email = db.EmailProperty()
     auth_id = db.StringProperty()
+    created = db.DateTimeProperty(auto_now_add=True)
 
 
 class Tag(db.Model):
@@ -15,10 +16,21 @@ class Tag(db.Model):
 
 
 class Post(db.Model):
-    url = db.StringProperty()
+    url = db.LinkProperty()
     title = db.StringProperty()
     body = db.StringProperty()
     img = db.StringProperty()
     tag = db.IntegerProperty()
     user = db.IntegerProperty()
     created = db.DateTimeProperty(auto_now_add=True)
+
+
+class User_to_Post(db.Model):
+    user_id = db.IntegerProperty()
+    post_ids = db.ListProperty(int)
+    posts_count = db.IntegerProperty()
+
+
+class Tag_to_Post(db.Model):
+    tag_id = db.IntegerProperty()
+    post_ids = db.ListProperty(int)
